e2e1411cc012fd39411b26703e13c75f391e37f2
Antriksh Chourasia <antrikshhii17@gmail.com>
2022-03-01 19:04:49 +0530

    Deleting dup

--- Linux_continued_pipeline/learn_bash/welcome.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-greeting="Welcome"
-user=$(whoami)
-day=$(date +%A)
-
-echo "$greeting back $user! Today is $day, which is the best day of the entire week"
-echo "Your bash shell version is: $BASH_VERSION. Enjoy!"
